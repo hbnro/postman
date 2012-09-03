725552b5ef180cbc55dec3d6d08da7700db0c851
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-09-03 15:33:29 -0500

    Block class for managing views

---
+++ lib/Postman/Block.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Postman;
+
+class Block
+{
+
+  private $name = '';
+  private $sections = array();
+
+
+  public function clear($name)
+  {
+    if (isset($this->sections[$name])) {
+      unset($this->sections[$name]);
+    }
+  }
+
+  public function section($name, $content)
+  {
+    $this->sections[$name] = array($content);
+  }
+
+  public function prepend($section, $content)
+  {
+    isset($this->sections[$section]) && array_unshift($this->sections[$section], $content);
+  }
+
+  public function append($section, $content)
+  {
+    isset($this->sections[$section]) && $this->sections[$section] []= $content;
+  }
+
+  public function yield($section, array $params = array())
+  {
+    if ( ! isset($this->sections[$section])) {
+      return; // TODO: raise exception
+    }
+
+    $out = '';
+
+    foreach ($this->sections[$section] as $one) {
+      if (is_callable($one)) {
+        ob_start() && call_user_func($one, $params);
+        $one = ob_get_clean();
+      }
+      $out .= $one;
+    }
+
+    return $out;
+  }
+
+  public function all(array $params = array())
+  {
+    $out = array();
+
+    foreach (array_keys($this->sections) as $one) {
+      $out[$one] = $this->yield($one, $params);
+    }
+
+    return $out;
+  }
+
+}
