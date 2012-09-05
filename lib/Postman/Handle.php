0d74fc4d62f1dad3845b4a79e034205347ae7e37
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-09-05 17:09:09 -0500

    Improve response handling for other content-types

--- lib/Postman/Handle.php
+++ lib/Postman/Handle.php
@@ -5,17 +5,42 @@ namespace Postman;
 class Handle
 {
 
+  private $type = '';
   private $klass = NULL;
   private $methods = array();
+  private $callbacks = array();
 
 
-  function __construct($obj)
+  function __construct($bundle, $type = '')
   {
-    $this->klass = $obj;
-    $this->methods = get_class_methods($obj);
+    $this->type = $type;
+    $this->klass = $bundle;
+    $this->methods = get_class_methods($bundle);
   }
 
 
+
+  public function responds($to, $data, array $params = array())
+  {
+    if ( ! empty($this->callbacks[$to])) {
+      $lambda = $this->callbacks[$to]['lambda'];
+      $params = $this->callbacks[$to]['params'] + $params;
+
+      return call_user_func($lambda, $data, $params);
+    }
+    // TODO: raise exception
+  }
+
+  public function register($type, \Closure $lambda, array $params = array())
+  {
+    if (is_array($type)) {
+      foreach ($type as $one) {
+        $this->response($one, $lambda, $params);
+      }
+    } else {
+      $this->callbacks[$type] = compact('lambda', 'params');
+    }
+  }
 
   public function execute($method, array $arguments = array())
   {
@@ -27,22 +52,26 @@ class Handle
       $test = call_user_func_array($callback, $arguments);
       $output = ob_get_clean();
 
-      $status = 200;
-      $headers = array();
-      $response = $output;
-
-      if (is_array($test)) {
-        if (is_string(key($test))) {
-          $headers = $test;
-        } else {
-          $status = array_shift($test) ?: $status;
-          $headers = array_shift($test) ?: $headers;
-          $response = array_shift($test) ?: $response;
-        }
-      } elseif (is_numeric($test)) {
-        $status = (int) $test;
+      if ($tmp = $this->responds($this->type, $test, compact('arguments'))) {
+        @list($status, $headers, $response) = $tmp;
       } else {
-        $response = (string) ($test ?: $output);
+        $status = 200;
+        $headers = array();
+        $response = $output;
+
+        if (is_array($test)) {
+          if (is_string(key($test))) {
+            $headers = $test;
+          } else {
+            $status = array_shift($test) ?: $status;
+            $headers = array_shift($test) ?: $headers;
+            $response = array_shift($test) ?: $response;
+          }
+        } elseif (is_numeric($test)) {
+          $status = (int) $test;
+        } else {
+          $response = (string) ($test ?: $output);
+        }
       }
 
       return array($status, $headers, $response);
